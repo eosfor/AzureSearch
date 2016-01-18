8ed183729fea51cfbbe5a091ee3c9541048713a2
Andrey Vernigora <andrey_vernigora@epam.com>
2016-01-18 12:21:52 +0300

    azure db cmdlets updated

--- azureDB.psm1
+++ azureDB.psm1
@@ -1,6 +1,27 @@
 ﻿function New-AzureObjectDB {
-param()
+    [cmdletbinding()]
+    param()
 process{
+    function getServices {
+        $sub.SubscriptionId | Invoke-Parallel { Invoke-RestMethod -uri https://management.core.windows.net/$_/services/hostedservices -Method GET -Headers $headers }
+    }
+    function getDeployments {
+    param($svc)
+        $svc.HostedServices.HostedService | skip-null | Invoke-Parallel -ThrottleLimit 16 {
+            $curr = $_
+            $subId = ([uri]$curr.Url).Segments[1] -replace "\/",""
+            try {
+                $dep = Invoke-RestMethod -uri "https://management.core.windows.net/$subId/services/hostedservices/$($curr.ServiceName)?embed-detail=true" -Method GET -Headers $headers 
+                $dep.HostedService.deployments.deployment | where {$_ -ne $null} | 
+                    Add-Member -MemberType NoteProperty -Name ServiceName -Value $curr.ServiceName -Force -PassThru |
+                    Add-Member -MemberType NoteProperty -Name SubscriptionID -Value $subId -Force -PassThru
+            }
+            catch {
+                $ErrorMessage = $_.Exception.Message
+                Write-Error "Failed to fetch data from $subId`:$($curr.ServiceName). Message: $ErrorMessage"
+            }
+        }
+    }
     $authHeader = Get-AzureAuthHeader
 
     $headerDate = '2014-10-01'
@@ -8,76 +29,56 @@ process{
                 "Authorization" = $authHeader;
                 'Accept' = 'application/json'}
 
-    # get subscriptions and services from them
+
     $sub = Get-AzureSubscription
 
-    $h = $sub.SubscriptionId | Invoke-Parallel { Invoke-RestMethod -uri https://management.core.windows.net/$_/services/hostedservices -Method GET -Headers $headers }
-
-    $ht = [hashtable]::Synchronized(@{})
-    $h.HostedServices.HostedService | skip-null| invoke-parallel {
-        #declare function inside of the script block
-        function xmlToObject {
-        param($o)
-        $h = @{}
-        $o  | gm -MemberType Property | % {
-            $prop = $_.name
-            if ($o."$prop" -is 'System.Xml.XmlElement') {
-                $h[$prop] = xmlToObject $o."$prop"
-            }
-            else { $h[$prop] = $o."$prop" }
-        }
-        $h
-        }
-        try {
-            $curr = $_
-            $subId = ([uri]$curr.Url).Segments[1] -replace "\/",""
-            $d = Invoke-RestMethod -uri "https://management.core.windows.net/$subId/services/hostedservices/$($curr.ServiceName)/deploymentslots/production" -Method GET -Headers $headers
-            if ($d) {
-                $ri1 = $d.Deployment.RoleInstanceList.RoleInstance | % { xmlToObject $_ }
-                $ro1 = $d.Deployment.RoleList.Role | % { xmlToObject $_  }
-                $ht[$curr.ServiceName] = $ri1 | % {$c = $_; $r = $c.'RoleName'; $x = $ro1 | where {$_.'RoleName' -eq $r};  $x.remove('RoleName'); [pscustomobject]($_ + $x) }
-            }
-            else { $ht[$curr.ServiceName] = $null }
-        }
-        catch{ $data = 1 }
-    }
+    $deployments = getDeployments (getServices)
 
     # prepare indexes
     $ipAddrIndex = @{}
     $storageAcctIndex = @{}
     $storageFileIndex = @{}
+    $servicesIndex = @{}
+    $deploymentIndex = @{}
 
-    #build indexes
-    $ht.Keys | % {
-        $ht[$_] | % {
-            if ($_.ipaddress) {$ipAddrIndex[$_.ipaddress] = $ipAddrIndex[$_.ipaddress] + (,$_)}
-            else {$ipAddrIndex["noip"] = $ipAddrIndex["noip"] + (,$_)}
-        }
-    }
-
-    $ht.Keys | % {
-        $ht[$_] | % {
-            if ($_.OSVirtualHardDisk) {$url = [uri]$_.OSVirtualHardDisk.MediaLink; $storageAcctIndex[$url.host] = $storageAcctIndex[$url.host] + (,$_)}
-            else {$storageAcctIndex["noDisk"] = $storageAcctIndex["noDisk"] + (,$_)}
-        }
-    }
-
-    $ht.Keys | % {
-        $ht[$_] | % {
+    $database = new-object System.Collections.ArrayList
+    
+    $deployments | % {
+        try {
             $curr = $_
-            if ($_.OSVirtualHardDisk) {$url = [uri]$_.OSVirtualHardDisk.MediaLink; $storageFileIndex[$url.Segments[-1]] = $storageFileIndex[$url.Segments[-1]] + (,$_)}
-            else {$storageFileIndex["noDrive"] = $storageFileIndex["noDrive"] + (,$_)}
+                $ri1 = $curr.RoleInstanceList.RoleInstance | % { xmlToObject $_ }
+                $ro1 = $curr.RoleList.Role | % { xmlToObject $_  }
+                $vm = $ri1 | % {$c = $_; $r = $c.'RoleName'; $x = $ro1 | where {$_.'RoleName' -eq $r};  $x.remove('RoleName'); [pscustomobject]($_ + $x) } | 
+                            Add-Member -MemberType NoteProperty -Name ServiceName -Value $curr.ServiceName -PassThru -Force |
+                            Add-Member -MemberType NoteProperty -Name DeploymentName -Value $curr.Name -PassThru -Force |
+                            Add-Member -MemberType NoteProperty -Name SubscriptionID -Value $curr.SubscriptionID -PassThru -Force
+                $vm | % {$database.Add($_)} | Out-Null
+                $servicesIndex[$curr.ServiceName] = $deploymentIndex[$curr.Name] = $vm                
+        }
+        catch{ $data = 1 }
+    }
 
-            if ($_.DataVirtualHardDisks) {
-                $_.DataVirtualHardDisks.DataVirtualHardDisk | % {
-                    $url = [uri]$_.MediaLink; $storageFileIndex[$url.Segments[-1]] = $storageFileIndex[$url.Segments[-1]] + (,$curr)
-                }
+    #build default indexes (IP, StorageAcct, VM os vhd, VM data VHD)
+    foreach ($element in $database){
+        if ($element.ipaddress) {$ipAddrIndex[$element.ipaddress] = $ipAddrIndex[$element.ipaddress] + (,$element)}
+            else {$ipAddrIndex["noip"] = $ipAddrIndex["noip"] + (,$element)}
+
+        if ($element.OSVirtualHardDisk) {$url = [uri]$element.OSVirtualHardDisk.MediaLink; $storageAcctIndex[$url.host] = $storageAcctIndex[$url.host] + (,$element)}
+            else {$storageAcctIndex["noDisk"] = $storageAcctIndex["noDisk"] + (,$element)}
+
+        if ($element.OSVirtualHardDisk) {$url = [uri]$element.OSVirtualHardDisk.MediaLink; $storageFileIndex[$url.Segments[-1]] = $storageFileIndex[$url.Segments[-1]] + (,$element)}
+            else {$storageFileIndex["noDrive"] = $storageFileIndex["noDrive"] + (,$element)}
+
+        if ($element.DataVirtualHardDisks) {
+            $element.DataVirtualHardDisks.DataVirtualHardDisk | % {
+                $url = [uri]$_.MediaLink; $storageFileIndex[$url.Segments[-1]] = $storageFileIndex[$url.Segments[-1]] + (,$curr)
             }
         }
+
     }
 
     #create database
-    $DB = @{Database = $ht; Index = @{'IPAddress' = $ipAddrIndex; StorageAccount = $storageAcctIndex; DriveFile = $storageFileIndex}}
+    $DB = @{Database = $database; Index = @{'IPAddress' = $ipAddrIndex; StorageAccount = $storageAcctIndex; DriveFile = $storageFileIndex; Service = $servicesIndex; Deployment = $deploymentIndex}}
     $DB
 }
 }
@@ -94,7 +95,6 @@ $o  | gm -MemberType Property | % {
 }
 $h
 }
-
 function Get-AzureObjectFromDB {
 [cmdletbinding()]
 param(
@@ -124,14 +124,8 @@ DynamicParam{
 }
 
 process {
-    $par = $PSBoundParameters
     if ($PSBoundParameters.IPAddress) {$Database.Index.IPAddress[$PSBoundParameters.IPAddress]}
     if ($PSBoundParameters.StorageAccount) {$Database.Index.StorageAccount[$PSBoundParameters.StorageAccount]}
     if ($PSBoundParameters.DriveFile) {$Database.Index.DriveFile[$PSBoundParameters.DriveFile]}
 }
 }
-
-
-# $db = New-AzureObjectDB
-# Get-AzureObjectFromDB -Database $DB -IPAddress 10.10.10.10
-# Get-AzureObjectFromDB -Database $DB -drivefile filename.vhd
